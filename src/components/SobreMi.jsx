553a77658e8806cf9813ee940d2fb6c24b083179
Joaquin <FacundoJoaGL@gmail.com>
2024-07-08 01:44:42 -0300

    quinto-commit

--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -5,7 +5,7 @@ export const SobreMi = () => {
     <div id="contenedor-sm">
 
       <h3 id="sm-titulo">Sobre Mi:</h3>
-      <p id="descripcion"> 23 años, autodidacta, curioso y responsable. Comprometido con el aprendizaje continuo y el desarrollo de soluciones tecnológicas efectivas. Excelente disposición para enfrentar desafíos laborales.</p>
+      <p id="descripcion"> 24 años, autodidacta, curioso y responsable. Comprometido con el aprendizaje continuo y el desarrollo de soluciones tecnológicas efectivas. Excelente disposición para enfrentar desafíos laborales.</p>
     
     </div>
     
