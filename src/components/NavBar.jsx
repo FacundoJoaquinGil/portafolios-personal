94017761c60925dce69b5e42e33c88b6d1152bff
FacundoJoaquinGil <FacundoJoaGL@gmail.com>
2024-10-22 14:12:38 -0300

    cambios variados

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,11 @@
 import { Nav, Navbar } from "react-bootstrap";
 
 export const NavBar = () => {
-  const handleNavClick = (e, path, id) => {
+  const handleNavClick = (e, id) => {
     e.preventDefault();
     const section = document.querySelector(id);
     if (section) {
       section.scrollIntoView({ behavior: "smooth" });
-      window.history.pushState(null, null, path); // Cambia la URL sin recargar
     }
   };
 
@@ -16,10 +15,10 @@ export const NavBar = () => {
         <Navbar.Toggle className="boton-nav" />
         <Navbar.Collapse id="navbar-collapse" className="justify-content-center">
           <Nav id="links" className="mx-auto justify-content-center">
-            <a href="/sobre-mi" className="nav-link" onClick={(e) => handleNavClick(e, '/sobre-mi', '#sobre-mi')}>Sobre Mi</a>
-            <a href="/proyectos" className="nav-link" onClick={(e) => handleNavClick(e, '/proyectos', '#proyectos')}>Proyectos</a>
-            <a href="/skills" className="nav-link" onClick={(e) => handleNavClick(e, '/skills', '#skills')}>Skills</a>
-            <a href="/contact" className="nav-link" onClick={(e) => handleNavClick(e, '/contact', '#contact')}>Contactos</a>
+            <a href="#sobre-mi" className="nav-link" onClick={(e) => handleNavClick(e, '#sobre-mi')}>Sobre Mi</a>
+            <a href="#proyectos" className="nav-link" onClick={(e) => handleNavClick(e, '#proyectos')}>Proyectos</a>
+            <a href="#skills" className="nav-link" onClick={(e) => handleNavClick(e, '#skills')}>Skills</a>
+            <a href="#contact" className="nav-link" onClick={(e) => handleNavClick(e, '#contact')}>Contactos</a>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
