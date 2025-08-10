9f44781aefb0580610dffc783b8e3a68c774ae26
FacundoJoaquinGil <FacundoJoaGL@gmail.com>
2025-08-10 19:42:35 -0300

    buenas practicas

--- src/components/HabilidadesTecnicas.jsx
+++ src/components/HabilidadesTecnicas.jsx
@@ -1,49 +1,13 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css/bundle";
-import css from "../assets/css.png";
-import html from "../assets/html.png";
-import js from "../assets/js.png";
-import react from "../assets/react.png";
-import bootstrap from "../assets/bootstrap.png";
-import node from "../assets/node.png";
-import sql from "../assets/sql.png";
-import git from "../assets/git-logo.png";
-import mongo from "../assets/mongo.png";
-import docker from "../assets/docker.png";
-import postman from "../assets/postman.png";
-import CSharp from "../assets/CSharp.png";
-import python from "../assets/python.png";
-import firebase from "../assets/firebase.png";
-import tsc from "../assets/tsc.png";
-import angular from "../assets/angular.png";
-import net from "../assets/net.png";
 import { useTranslation } from "react-i18next";
 import { VerTodasTecnologias } from "./VerTodasTecnologias";
+import { skills } from "../data/skillsData";
 
 export const HabilidadesTecnicas = () => {
 
   const { t } = useTranslation();
-
-  const skills = [
-    { src: html, alt: "HTML" },
-    { src: css, alt: "CSS" },
-    { src: js, alt: "JavaScript" },
-    { src: react, alt: "React" },
-    { src: bootstrap, alt: "Bootstrap" },
-    { src: node, alt: "Node.js" },
-    { src: sql, alt: "SQL" },
-    { src: CSharp, alt: "C#" },
-    { src: mongo, alt: "MongoDB" },
-    { src: docker, alt: "Docker" },
-    { src: postman, alt: "Postman" },
-    { src: python, alt: "python" },
-    { src: firebase, alt: "firebase" },
-    { src: tsc, alt: "tsc" },
-    { src: git, alt: "git" },
-    { src: angular, alt: "angular" },
-     { src: net, alt: ".NET" },
-  ];
 
   return (
     <>
---
+++ src/data/skillsData.js
@@ -0,0 +1,37 @@
+import css from "../assets/css.png";
+import html from "../assets/html.png";
+import js from "../assets/js.png";
+import react from "../assets/react.png";
+import bootstrap from "../assets/bootstrap.png";
+import node from "../assets/node.png";
+import sql from "../assets/sql.png";
+import git from "../assets/git-logo.png";
+import mongo from "../assets/mongo.png";
+import docker from "../assets/docker.png";
+import postman from "../assets/postman.png";
+import CSharp from "../assets/CSharp.png";
+import python from "../assets/python.png";
+import firebase from "../assets/firebase.png";
+import tsc from "../assets/tsc.png";
+import angular from "../assets/angular.png";
+import net from "../assets/net.png";
+
+export const skills = [
+  { src: html, alt: "HTML" },
+  { src: css, alt: "CSS" },
+  { src: js, alt: "JavaScript" },
+  { src: react, alt: "React" },
+  { src: bootstrap, alt: "Bootstrap" },
+  { src: node, alt: "Node.js" },
+  { src: sql, alt: "SQL" },
+  { src: CSharp, alt: "C#" },
+  { src: mongo, alt: "MongoDB" },
+  { src: docker, alt: "Docker" },
+  { src: postman, alt: "Postman" },
+  { src: python, alt: "Python" },
+  { src: firebase, alt: "Firebase" },
+  { src: tsc, alt: "TypeScript" },
+  { src: git, alt: "Git" },
+  { src: angular, alt: "Angular" },
+  { src: net, alt: ".NET" },
+]; 
