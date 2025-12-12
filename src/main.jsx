8bbcff4e5ae0eb261fd1ca67a948e5c5cd468694
FacundoJoaquinGil <FacundoJoaGL@gmail.com>
2025-12-12 11:29:59 -0300

    snowfall

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './App'
-import './styles/index.css'
-import './i18n';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+import "./styles/index.css";
+import "./i18n";
+import Snowfall from "react-snowfall";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
-)
+    <Snowfall snowflakeCount={100}/>
+    <App />
+  </React.StrictMode>
+);
 
 // import ReactDOM from "react-dom/client";
 // import { App } from './App'
