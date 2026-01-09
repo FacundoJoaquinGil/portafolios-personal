d590e5b3dc8e60728b0b00ecd890b8419e61c415
FacundoJoaquinGil <FacundoJoaGL@gmail.com>
2026-01-09 11:07:44 -0300

    clear snowfall

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,9 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles/index.css";
 import "./i18n";
-import Snowfall from "react-snowfall";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Snowfall snowflakeCount={100}/>
     <App />
   </React.StrictMode>
 );
